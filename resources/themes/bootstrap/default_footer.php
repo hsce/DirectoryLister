209c6bd504f4e79fc0755c66d44fe55e416624ce
Cyril Wong <kli@live.com>
2018-08-29 12:15:30 +0800

    Directory Lister

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,6 @@
+<hr>
+
+<div class="footer">
+    <p> <a href="http://www.lia.im/">Hello world</a> - Powered by Directory Lister - Hosting by <a href="https://clientarea.ramnode.com/aff.php?aff=4217" target="_blank" rel="nofollow">RamNode</a> - <a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow">粤ICP备18021459号-2</a>
+
+</div>
