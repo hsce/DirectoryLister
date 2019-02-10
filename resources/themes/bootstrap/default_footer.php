675f649170a35a0d5f63ccb4a59ea91f6a18ca76
Cyril Wong <kli@live.com>
2019-02-10 17:34:18 +0800

    Update default_footer.php

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -7,5 +7,5 @@
   document.write(new Date().getFullYear());
 // ]]></script>
         <!-- 自动更新版权时间代码结束 -->
-        <a href="https://www.mliy.net/" title="Carignor" target="_blank">QiMeng</a> ♡ <a href="https://github.com/ikym/DirectoryLister" title="Caringor" target="_blank">DirectoryLister</a> .</div>
+        <a href="https://www.mlio.net/" title="Carignor" target="_blank">Hello World</a> ♡ <a href="https://github.com/ikym/DirectoryLister" title="Caringor" target="_blank">DirectoryLister</a> .</div>
 </div>
